42b72ec7ebeb447795e2537f87322e78f440a9a2
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2020-02-16 19:53:54 +0100

    navigation basics

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Projects from "./pages/Projects";
 
 export const Palette = {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,19 @@
 import React from "react";
 import "./Header.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default class Header extends React.Component {
   render() {
     return (
-      <Router>
-        <div className="Header" data-testid="header">
-          <div className="Header-bar">
-            <Link to="/">
-              <b>ALESSIO BIANCHETTI</b>
-            </Link>
-            <Link to="/projects">
-              <b>Projects</b>
-            </Link>
-          </div>
+      <div className="Header" data-testid="header">
+        <div className="Header-bar">
+          <a href="/">
+            <b>ALESSIO BIANCHETTI</b>
+          </a>
+          <a href="/projects">
+            <b>Projects</b>
+          </a>
         </div>
-      </Router>
+      </div>
     );
   }
 }
